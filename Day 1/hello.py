83c2563daf0126ada215e7fb9b005a3f598a0013
James Sweetsir <jsweetsir18@icloud.com>
2025-04-23 17:35:48 -0400

    update

--- Day 1/hello.py
+++ Day 1/hello.py
@@ -8,11 +8,3 @@ def greeting():
     print("Hello " + name)
 
 greeting()
-
-def add():
-    num1 = int(input("Enter the first number: "))
-    num2 = int(input("Enter the second number :"))
-    answer = num1 + num2
-    print(answer)
-
-add()
