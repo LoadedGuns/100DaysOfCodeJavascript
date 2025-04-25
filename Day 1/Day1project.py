aa80a9561bd5a03f83e9ea08dab5ccb3bd3add2c
James Sweetsir <jsweetsir18@icloud.com>
2025-04-24 21:59:21 -0400

    Day 1

---
+++ Day 1/Day1project.py
@@ -0,0 +1,9 @@
+import time
+
+print("Welcome to the band name generator!")
+time.sleep(1)
+city = input("What city did you grow up in?\n")
+time.sleep(1)
+pet = input("What is your pet's name?\n")
+time.sleep(1)
+print("Your band name is " + city + ", " + pet)
